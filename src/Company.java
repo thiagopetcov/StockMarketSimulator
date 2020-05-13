d4e7133945b13536895eada8fa94d8f646fce5c4
thiagopetcov <thiago.petcov@gmail.com>
2020-05-13 21:22:14 +0100

    Array List

--- src/Company.java
+++ src/Company.java
@@ -1,29 +1,26 @@
 public class Company {
 
 	// Declare the attributes the company will have
-	private int c_id;
+	private int id;
 	private int shares;
 	private double price;
-	private double capital;
-	private int sharesSold;
 
 	// Define a private company constructor
-	private Company(int c_id, int shares, double price, double capital, int sharesSold) {
-		this.c_id = c_id;
+	private Company(int id, int shares, double price) {
+		this.id = id;
 		this.shares = shares;
 		this.price = price;
-		this.capital = capital;
 
 	}
 
 	// provide getters and setters
 	// to be able to access or modify the values
 	public int getCid() {
-		return c_id;
+		return id;
 	}
 
-	public void setCid(int c_id) {
-		this.c_id = c_id;
+	public void seid(int id) {
+		this.id = id;
 	}
 
 	public int getShares() {
@@ -42,51 +39,32 @@ public class Company {
 		this.price = price;
 	}
 
-	public double getCapital() {
-		return capital;
-	}
-
-	public void setCapital(double capital) {
-		this.capital = capital;
-	}
-
-	public int getSharesSold() {
-		return sharesSold;
-	}
-
-	public void setSharesSold(int SharesSold) {
-		this.sharesSold = sharesSold;
-	}
 
 	// Provide a toString method in order to print all the values of the objects
 	@Override
 	public String toString() {
-		return "Company ID: " + c_id + " No of Shares: " + shares + " Share Price: " + price + " Capital: " + capital
-				+ " Shares Sold: " + sharesSold;
+		return "Company ID: " + id + " No of Shares: " + shares + " Share Price: " + price;
 	}
 
 	// Create a nested static Company builder class
 	public static class CompanyBuilder {
 
-		private int c_id;
+		private int id;
 		private int shares;
 		private double price;
-		private double capital;
-		private int sharesSold;
 
 		// Create a constructor for the inner class
-		public CompanyBuilder(int c_id, int shares, double price, double capital, int sharesSold) {
-			this.c_id = c_id;
+		public CompanyBuilder(int id, int shares, double price) {
+			this.id = id;
 			this.shares = shares;
 			this.price = price;
-			this.capital = capital;
-			this.sharesSold = sharesSold;
 		}
+	
 
 		// This is the actual method that creates the object
 		// passing the values to the outer class
 		public Company build() {
-			return new Company(c_id, shares, price, capital, sharesSold);
+			return new Company(id, shares, price);
 		}
 
 	}
--- src/Investor.java
+++ src/Investor.java
@@ -1,30 +1,41 @@
-import java.util.ArrayList;
+ import java.util.ArrayList;
 import java.util.HashSet;
 
 
 public class Investor {
 
 	// Declare the attributes the Investor will have
-	private int i_id;
+	private int id;
 	private double budget;
-	private int i_shares;
+	private String Investor;
+	
+
+    String[] shares = new String[]{"ARM", "IBM", "Google", "Facebook", "Twitter", "Samsung", "Lenovo", "Intel"};
+
+    ArrayList list = (ArrayList) Arrays.asList(shares);
+
+    System.out.println("String array converted to List");
+
+    for(int i=0; i < list.size(); i++)
+    {
+        System.out.println(list.get(i));
+    }
 
 	// Define a private investor constructor
-	private Investor(int i_id, double budget, int i_shares) {
-		this.i_id = i_id;
+	private Investor(int id, double budget) {
+		this.id = id;
 		this.budget = budget;
-		this.i_shares = i_shares;
-
+		
 	}
 
 	// provide getters and setters
 	// to be able to access or modify the values
 	public int getIid() {
-		return i_id;
+		return id;
 	}
 
 	public void setIid(int i_id) {
-		this.i_id = i_id;
+		this.id = i_id;
 	}
 
 	public double getBudget() {
@@ -35,39 +46,29 @@ public class Investor {
 		this.budget = budget;
 	}
 
-	public int getIShares() {
-		return i_shares;
-	}
-
-	public void setIShares(int i_shares) {
-		this.i_shares = i_shares;
-	}
-
 	// Provide a toString method in order to print all the values of the objects
 	@Override
 	public String toString() {
-		return "Ivestor ID: " + i_id + " Budget: " + budget + " No of invested Shares: " + i_shares;
+		return "Ivestor ID: " + id + " Budget: " + budget;
 	}
 
 	// Create a nested static Investor builder class
 	public static class InvestorBuilder {
 
-		private int i_id;
+		private int id;
 		private double budget;
-		private int i_shares;
 
 		// Create a constructor for the inner class
-		public InvestorBuilder(int i_id, double budget, int i_shares) {
-			this.i_id = i_id;
+		public InvestorBuilder(int id, double budget) {
+			this.id = id;
 			this.budget = budget;
-			this.i_shares = i_shares;
 
 		}
 
 		// This is the actual method that creates the object
 		// passing the values to the outer class
 		public Investor build() {
-			return new Investor(i_id, budget, i_shares);
+			return new Investor(id, budget);
 		}
 
 	}
